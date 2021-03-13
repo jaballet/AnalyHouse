9671dd07671c6b7325908ac72ba04f58db6169e4
kjky12 <kjky123@naver.com>
2021-03-13 14:05:13 +0900

    소스코드로 openapi확인 이어서 xml 파싱하기

--- AnalyHouse/AnalyHouse.py
+++ AnalyHouse/AnalyHouse.py
@@ -1,5 +1,30 @@
 
+import requests
+import xml.etree.ElementTree as elemTree
 
-a = 0
 
-b= a
+## 공공데이터 포털 
+URL = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade'
+
+##국토교통부_아파트매매 실거래자료
+#서비스키
+ServiceKey = "sEkWhWER%2Fsn%2FgRDrtuBW2BHO%2BE65ORsVQQuDOqMnvZOa%2Fh6Ir0L6ppED3VIkx%2BqSEFdkhygEpOHsOQfQqzq56w%3D%3D"
+# 지역별 코드 Sample : 11110
+LAWD_CD = "LAWD_CD"
+# 월단위 신고 자료 Sample : 201512
+DEAL_YMD = "DEAL_YMD"
+
+strSetup = "?serviceKey={0}&{1}=11110&{2}=201512".format(ServiceKey, LAWD_CD, DEAL_YMD)
+response = requests.get(URL + strSetup)
+response.status_code
+print(response.text)
+
+
+#xml 파싱
+tree = elemTree(response.text)
+root = tree.getroot()
+
+
+a =  0
+
+
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # AnalyHouse
 부동산 데이터 분석 프로젝트
 
+
+2021-03-13
+명령프롬프트 : C:\#Project\GIT\AnalyHouse\AnalyHouse\AnalyHous\Scripts 경로로 이동해서 
+명령어 "jupyter notebook"
+
 2021-03-06
 1차 : 부동산 & 경매 낙찰액을 비교할 수 있도록!
  - 공공데이터포탈에서 호가(이게 될진 모르겠다), 실거래가에 대한 정보를 얻어와서 차트에 표시할 수 있도록 데이터를 수집하자
